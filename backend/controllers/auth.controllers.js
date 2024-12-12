29458e80651e8276daf75e9c476bea530b9fafe8
sseeker05 <sseeker05@gmail.com>
2024-12-12 13:04:43 -0500

    controllers set

---
+++ backend/controllers/auth.controllers.js
@@ -0,0 +1,12 @@
+export const signup = async (req,res)=> {
+  res.send("sign up route called")
+}
+
+export const login = async (req,res)=> {
+  res.send("login up route called")
+}
+
+export const logout = async (req,res)=> {
+  res.send("logout up route called")
+}
+
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
 import express from "express"
 import dotenv from "dotenv"
+import authRoutes from "./routes/auth.route.js"
+
 
 dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 50001
+
+
+app.use("/api/auth", authRoutes)
+
+
 
 app.listen(5001,()=> {
   console.log("Server is running on http://localhost:"+ PORT)
